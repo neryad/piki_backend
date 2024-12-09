c55739153602adbf229d92c308b1ee7251670d20
neryad <dayern.gomez@hotmail.com>
2024-12-08 20:32:55 -0400

    a

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
 import "dotenv/config";
 
 import { router } from "./routes/index.js";
@@ -7,5 +8,6 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors());
+app.use(helmet());
 app.use(router);
-app.listen(PORT, () => console.log("Api its up port"));
+app.listen(PORT, () => console.log(`Api its up port ${PORT}`));
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,20 +3,28 @@ import { readdirSync } from "fs";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 
-// Crear equivalentes de __filename y __dirname
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const PATH_ROUTER = __dirname; // Ahora usamos el equivalente
+const PATH_ROUTER = __dirname;
 
 const router = Router();
 
+/**
+ * The cleanFileName function removes the file extension from a given file name.
+ * @param fileName - The `cleanFileName` function takes a `fileName` as a parameter and removes the
+ * file extension from it.
+ * @returns The cleanFileName function returns the file name without the file extension.
+ */
 const cleanFileName = (fileName) => {
   const file = fileName.split(".").shift();
   return file;
 };
 
-// Leer archivos en el directorio
+/* This code snippet is reading the contents of a directory specified by `PATH_ROUTER` using
+`readdirSync`, then filtering the filenames based on a condition. For each filename that meets the
+condition (where the clean name is not "index"), it dynamically imports a module and adds its router
+to the main router. */
 readdirSync(PATH_ROUTER).filter((filename) => {
   const cleanName = cleanFileName(filename);
 
