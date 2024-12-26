d49b6e7cd9218a8bb2fdc30d2637cf106878af4f
neryad <dayern.gomez@hotmail.com>
2024-12-26 16:19:31 -0400

    Add user management functions: retrieve all users, get user by ID, update user, and delete user

--- src/controllers/users/user.js
+++ src/controllers/users/user.js
@@ -25,3 +25,80 @@ export const createUser = async (req, res) => {
     res.status(500).json({ error: "Error al crear el usuario" });
   }
 };
+
+export const getAllUsers = async (req, res) => {
+  try {
+    const response = await turso.execute("SELECT * FROM users;");
+
+    res.status(200).json(response.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error al obtener los usuarios" });
+  }
+};
+
+export const getUserById = async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  console.log(req.params);
+  console.log(req);
+  try {
+    const response = await turso.execute("SELECT * FROM users WHERE id = ?;", [
+      id,
+    ]);
+
+    if (response.rows.length === 0) {
+      return res.status(404).json({ error: "Usuario no encontrado" });
+    }
+    res.status(200).json(response.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error al obtener el usuario" });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  const { id } = req.params;
+  const { name, lastName, phone, email, password, role_id } = req.body;
+
+  try {
+    const saltRounds = 10;
+    const hashedPassword = password
+      ? await bcrypt.hash(password, saltRounds)
+      : undefined;
+
+    const query = `
+      UPDATE users
+      SET name = ?, lastName = ?, phone = ?, email = ?, ${
+        hashedPassword ? "password = ?, " : ""
+      } role_id = ?
+      WHERE id = ?;
+    `;
+    const params = [
+      name,
+      lastName,
+      phone,
+      email,
+      hashedPassword,
+      role_id,
+      id,
+    ].filter((param) => param !== undefined);
+
+    await turso.execute(query, params);
+    res.status(200).json({ message: "Usuario actualizado exitosamente" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error al actualizar el usuario" });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    await turso.execute("DELETE FROM users WHERE id = ?;", [id]);
+    res.status(200).json({ message: "Usuario eliminado exitosamente" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error al eliminar el usuario" });
+  }
+};
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,20 @@
 import { Router } from "express";
-import { createUser } from "../controllers/users/user.js";
+import {
+  createUser,
+  getAllUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+} from "../controllers/users/user.js";
 const router = Router();
 
 router.get("/", (req, res) => {
   return res.send("USers");
 });
 router.post("/", createUser);
+router.get("/allUsers", getAllUsers);
+router.get("/:id", getUserById);
+router.put("/:id", updateUser); // Ruta con parámetro para actualizar un usuario
+router.delete("/:id", deleteUser); //
 
 export { router };
