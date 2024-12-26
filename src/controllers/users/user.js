00a9ebb50293ef69420a47b8c43f59928b131457
neryad <dayern.gomez@hotmail.com>
2024-12-26 15:46:03 -0400

    Add bcrypt for password hashing and validate user input in createUser function

--- src/controllers/users/user.js
+++ src/controllers/users/user.js
@@ -1,15 +1,23 @@
 import { turso } from "../../db/db.js";
+import bcrypt from "bcrypt";
 
 export const createUser = async (req, res) => {
   let { name, lastName, phone, email, password, role_id, created_at } =
     req.body;
+
+  if (!name || !lastName || !phone || !email || !password || !role_id) {
+    return res.status(400).json({ error: "Todos los campos son obligatorios" });
+  }
+
   let currentDate = new Date();
   created_at = currentDate.toISOString().slice(0, 19).replace("T", " ");
 
   try {
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
     await turso.execute(
       "INSERT INTO users (name, lastName, phone, email, password, role_id, created_at) VALUES (?, ?, ?,?,?,?,?);",
-      [name, lastName, phone, email, password, role_id, created_at]
+      [name, lastName, phone, email, hashedPassword, role_id, created_at]
     );
     res.status(201).json({ message: "Usuario creado exitosamente" });
   } catch (error) {
