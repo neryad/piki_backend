d58cf29e61f7d7cdd57ee2370888ea715c98b9aa
neryad <dayern.gomez@hotmail.com>
2024-12-26 21:18:28 -0400

    Add custom 404 error page and middleware for handling non-existent routes

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 import cors from "cors";
 import helmet from "helmet";
 import "dotenv/config";
 
 import { router } from "./routes/index.js";
 import initializeDatabase from "./db/initdb.js";
-const PORT = process.env.PORT || 3000;
 
+const PORT = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -19,5 +23,9 @@ initializeDatabase()
   .catch((error) => {
     console.log(error);
   });
+// Middleware para manejar rutas no existentes
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "../public/404.html"));
+});
 app.use(router);
 app.listen(PORT, () => console.log(`Api its up port ${PORT}`));
